32726c5437bb5045be4b3678e254964048301240
brunocardosof <bruno.cardosof@gmail.com>
2021-01-17 15:29:44 -0300

    add .env to hide email sensitivy informations

--- source/Config.php
+++ source/Config.php
@@ -1,11 +1,16 @@
 <?php
+require $_SERVER['DOCUMENT_ROOT']."/e-lastic-case-php/vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
 define("MAIL",[
-  "host" => "smtp.sendgrid.net",
-  "port" => 587,
-  "user" => "apikey",
-  "password" => "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",
-  "from_name" => "Bruno Cardoso",
-  "from_email" => "bruno.cardosof@gmail.com",
+  "host" => $_ENV['HOST'],
+  "port" => $_ENV['PORT'],
+  "user" => $_ENV['USER'],
+  "password" => $_ENV['PASSWORD'],  
+  "from_name" => $_ENV['FROM_NAME'],
+  "from_email" => $_ENV['FROM_EMAIL'],
 ]);
 
 define("BASE_URL", "http://localhost/e-lastic-case-php");
