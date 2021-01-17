80d99162412db1b7a3981ade00e908a8a0471a8b
brunocardosof <bruno.cardosof@gmail.com>
2021-01-17 17:30:10 -0300

    add render body email html method

--- source/Controller/TrackAndSendEmailController.php
+++ source/Controller/TrackAndSendEmailController.php
@@ -11,6 +11,7 @@ class TrackAndSendEmailController{
   
   public function send($data){  
     $track = $this->track($data["trackCode"]);
+    $bodyHTML = $this->renderBodyEmail($track, $data["trackCode"]);
     if(isset($_FILES['pdf'])) {
       $this->hasAttach = true;
       $path = PATH_UPLOADS_PDF;
@@ -23,7 +24,7 @@ class TrackAndSendEmailController{
     if($this->hasAttach){      
     $emailProvider->add(
       $data["subject"],
-      $data["body"],
+      $bodyHTML,
       $data["recipient_name"],
       $data["recipient_email"],
     // )->send();
@@ -51,5 +52,93 @@ class TrackAndSendEmailController{
     $correios = new CorreiosConsulta();
     $out = $correios->rastrear($trackCode);
     return $out;
+  }
+
+  private function renderBodyEmail($track, $trackCode){
+    $html = "
+    <html>
+    <head>
+      <style>  
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      } 
+      .container{
+      }        
+      .wrapper{
+        justify-content: center;
+        align-items: center;
+      }        
+      .titleCardEmailSender{
+        margin-bottom: 30px;
+        text-align: center;
+      }
+      .subTitleCardEmailSender {
+        margin-bottom: 15px;
+        text-align: center;
+        color: #e34a5b !important;
+      }
+      .subTitleCardEmailSender a{
+        margin-bottom: 15px;
+        text-align: center;
+        color: #e34a5b !important;
+        font-size: 20px;
+      }
+      .cardListTrack {
+        margin: 15px auto 15px auto;
+        border: 1.5px solid #e34a5b;
+        border-radius: 5px;
+        width: 50%;
+      }
+      ul {
+        list-style-type: none;
+      }
+      h1, h4, li {
+        color: #383838 !important;
+      }
+      li {
+        padding: 5 0px 5 0px;
+      }
+      .cardListTrack{
+        text-align: center !important;
+      }
+    @media (max-width:480px)  {
+      .cardListTrack {
+        width: 100% !important;
+      }
+    }
+    @media (max-width:960px)  {
+      .cardListTrack {
+        width: 100% !important;
+      }
+    }
+    </style>
+    </head>";
+      $html .= "<div class='container'>";
+        // $html .= "<div class='wrapper'>";
+          $html .= "<h1 class='titleCardEmailSender'>Histórico do Objeto</h1>";
+          $html .= "<h4 class='subTitleCardEmailSender'>Acompanhe o rastreio do objeto <a href='https://www2.correios.com.br/sistemas/rastreamento/default.cfm'>{$trackCode}</a></h4>";
+          $html .= "<ul id='ulTrack'> ";
+          foreach($track as $key => $value) {
+            $html .= "<div class='cardListTrack'>";
+              $html .= "<li>";
+                $html .= "Status: ".$value['status'];
+              $html .= "</li>";
+
+              $html .= "<li>";
+                $html .= "Data: ". $value['data'];
+              $html .= "</li>";
+
+              $html .= "<li>";
+                $html .= "Local: ". $value['local'];
+              $html .= "</li>";
+            $html .= "</div>";
+          }
+          $html .= "</ul>";
+        // $html .= "</div>";
+      $html .= "</div>";
+    $html .= "</html>";
+    return $html;
   }
 }
